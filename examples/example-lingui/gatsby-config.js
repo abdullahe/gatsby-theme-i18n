7c7cb34df20a60d11906bfd7964c1515d0c27fce
Lennart <lekoarts@gmail.com>
2020-06-23 10:53:27 +0200

    feat: gatsby-theme-i18n-lingui (#4)

---
+++ examples/example-lingui/gatsby-config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://gatsby-theme-i18n.netlify.app`,
+    title: "gatsby-theme-i18n",
+    description: `Default example for i18n`,
+    author: `LekoArts`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: `pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: `blog`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-theme-i18n`,
+      options: {
+        defaultLang: `en`,
+        configPath: require.resolve(`./i18n/config.json`),
+      },
+    },
+    {
+      resolve: `gatsby-theme-i18n-lingui`,
+      options: {
+        localeDir: `./i18n/lingui`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve(`./src/components/layout.js`),
+        },
+      },
+    },
+  ],
+}
